2fd2effa0319b38b1b45b3375627e28978f414c8
Serge Smertin <serge.smertin@databricks.com>
2022-07-07 12:43:18 +0200

    improve scripts

--- scripts/tfit-env.py
+++ scripts/tfit-env.py
@@ -1,8 +1,8 @@
-''' Turn all secrets in the current GitHub Actions Environment to environment variables
+''' Turn all secrets in the current GitHub Actions Environment to uppercased environment variables
 
 USAGE: 
     
-    python3 scripts/tfit-env.py ${{ inputs.environment }} ${{ toJson(secrets) }} >> $GITHUB_ENV
+    python3 scripts/tfit-env.py ${{ inputs.environment }} '${{ toJson(secrets) }}' >> $GITHUB_ENV
 
 Docs: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
 '''
@@ -13,4 +13,9 @@ secrets = sys.argv[2]
 
 print(f'CLOUD_ENV={env.split("-")[0]}')
 for k,v in json.loads(secrets).items():
+    if k == 'github_token':
+        # this is an internal token for github actions.
+        # skipping it to avoid potential confusion.
+        # perhaps it might be useful in some cases.
+        continue
     print(f'{k.upper()}={v}')
