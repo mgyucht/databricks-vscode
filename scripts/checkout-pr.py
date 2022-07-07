7abd91003c73cb36c4a1839187ae49c14010c2fb
Serge Smertin <serge.smertin@databricks.com>
2022-07-07 12:30:13 +0200

    try PR verify workflow

---
+++ scripts/checkout-pr.py
@@ -0,0 +1,24 @@
+#!/bin/env python3
+
+import os, sys
+from ghapi.all import GhApi
+
+pull_request_number = sys.argv[1]
+print(f'INFO: Looking for PR#{pull_request_number}')
+
+OWNER = 'databricks'
+REPO = 'terraform-provider-databricks'
+
+try:
+    gh = GhApi(owner=OWNER, repo=REPO)
+    pr = gh.pulls.get(pull_request_number)
+    
+    ref = pr['head']['ref']
+    clone_url = pr['head']['repo']['clone_url']
+
+    command = f'git clone {clone_url} --branch {ref} ext/terraform-provider-databricks'
+    print(f'INFO: Checking out PR for the build: {command}')
+    print(os.system(command))
+except Exception as e:
+    sys.stderr.write(f'ERROR: {e}')
+    sys.exit(1)
