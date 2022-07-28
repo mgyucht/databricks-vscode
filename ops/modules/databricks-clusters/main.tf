aae5771e9ac737021d3e0325bddc14938c537043
Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>
2022-07-28 14:58:25 +0100

    Update ops/modules/databricks-clusters/main.tf
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- ops/modules/databricks-clusters/main.tf
+++ ops/modules/databricks-clusters/main.tf
@@ -14,7 +14,7 @@ resource "databricks_cluster" "default_test_cluster" {
   cluster_name            = module.defaults.cluster_names.default_cluster_name
   spark_version           = data.databricks_spark_version.latest_lts.id
   node_type_id            = data.databricks_node_type.smallest.id
-  autotermination_minutes = 120
+  autotermination_minutes = 60
   autoscale {
     min_workers = 1
     max_workers = 50
