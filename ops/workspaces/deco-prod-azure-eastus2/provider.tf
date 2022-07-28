3bf484277097cba76ff570c71f14aa3162e711ee
kartikgupta-db <kartik.gupta@databricks.com>
2022-07-28 14:38:26 +0100

    github->gh

--- ops/workspaces/deco-prod-azure-eastus2/provider.tf
+++ ops/workspaces/deco-prod-azure-eastus2/provider.tf
@@ -21,8 +21,8 @@ locals {
   can_of_worms = data.terraform_remote_state.meta.outputs.secrets
 }
 provider "databricks" {
-  host                = local.can_of_worms["deco-github-azure-prod:DATABRICKS-HOST"]
-  azure_client_id     = local.can_of_worms["deco-github-azure-prod:ARM-CLIENT-ID"]
-  azure_client_secret = local.can_of_worms["deco-github-azure-prod:ARM-CLIENT-SECRET"]
-  azure_tenant_id     = local.can_of_worms["deco-github-azure-prod:ARM-TENANT-ID"]
+  host                = local.can_of_worms["deco-gh-azure-prod:DATABRICKS-HOST"]
+  azure_client_id     = local.can_of_worms["deco-gh-azure-prod:ARM-CLIENT-ID"]
+  azure_client_secret = local.can_of_worms["deco-gh-azure-prod:ARM-CLIENT-SECRET"]
+  azure_tenant_id     = local.can_of_worms["deco-gh-azure-prod:ARM-TENANT-ID"]
 }
