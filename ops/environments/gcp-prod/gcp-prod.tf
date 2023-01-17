24c94577b89bb28853a414e383e30e976b65384f
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-01-17 17:41:41 +0100

    Remove DATABRICKS_GOOGLE_SERVICE_ACCOUNT from gcp-prod (#170)
    If set, the `google-id` auth provider kicks in and fails. We intend to
    use the `google-credentials` auth provider here.

--- ops/environments/gcp-prod/gcp-prod.tf
+++ ops/environments/gcp-prod/gcp-prod.tf
@@ -50,7 +50,6 @@ module "secrets" {
     {
       "CLOUD_ENV" : "gcp",
       "DATABRICKS_HOST" : databricks_mws_workspaces.workspace.workspace_url,
-      "DATABRICKS_GOOGLE_SERVICE_ACCOUNT" : module.service_account.email,
       "GOOGLE_CREDENTIALS" : google_service_account_key.this.private_key,
     },
   )
